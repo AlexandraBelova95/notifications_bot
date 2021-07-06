ceef260b14a4207c461963abc81398c041180838
alex <alexandrabelova1504@gmail.com>
2021-07-06 13:03:41 +0500

    Exceptions changed, decoding of JSON optimized, number of messages decreased, PEP8 applied

--- tg_notifications_bot.py
+++ tg_notifications_bot.py
@@ -18,13 +18,13 @@ def main():
     timestamp = time.time()
 
     headers = {
-    "Authorization": devman_token
+        "Authorization": devman_token
     }
     payload = {
-    timestamp: timestamp
+        timestamp: timestamp
     }
 
-    times_exceptions_appear = 0;
+    times_exceptions_appear = 0
 
     while True:
         try:
@@ -35,35 +35,35 @@ def main():
                 params=payload
             )
             response.raise_for_status()
-            response_dict = response.json()
-            if response_dict["status"] == "timeout":
-                timestamp = response_dict["timestamp_to_request"]
-            elif response_dict["status"]== "found":
-                timestamp = response_dict["last_attempt_timestamp"]
+            review = response.json()
+            if review["status"] == "timeout":
+                timestamp = review["timestamp_to_request"]
+            elif review["status"] == "found":
+                timestamp = review["last_attempt_timestamp"]
                 chat_id = os.getenv("TG_CHAT_ID")
-                last_new_attempt = response_dict['new_attempts'][0]
+                last_new_attempt = review['new_attempts'][0]
                 work_name = last_new_attempt['lesson_title']
                 work_url = last_new_attempt['lesson_url']
                 if last_new_attempt["is_negative"]:
                     final_message = "К сожалению, в работе нашлись ошибки."
-                elif last_new_attempt["is_negative"]:
+                else:
                     final_message = "Преподавателю все понравилось,"
                     + "можно приступать к следующему уроку!"
                 bot.send_message(
-                    chat_id, 
-                    text="Преподаватель проверил работу\n"+
-                    f"'{work_name}'\ndvmn.org{work_url}" +
-                    f"\n{final_message}"
+                    chat_id,
+                    text=f"""Преподаватель проверил работу:
+                    '{work_name}'
+                    dvmn.org{work_url}
+                    {final_message}"""
                     )
-        except (
-            requests.exceptions.ReadTimeout,
-            requests.exceptions.ConnectionError
-        ):
+        except requests.exceptions.ConnectionError:
             if times_exceptions_appear > 10:
                 time.sleep(10)
                 times_exceptions_appear = 0
-            else: 
-                times_exceptions_appear+=1
+            else:
+                times_exceptions_appear += 1
+        except requests.exceptions.ReadTimeout:
+            continue
 
 if __name__ == '__main__':
     main()
